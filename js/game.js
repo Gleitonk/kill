300737fef36673aec9b740d61836293993bfdafd
Gleitonk <81481231+Gleitonk@users.noreply.github.com>
2022-01-28 14:26:07 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,18 +1,22 @@
 var height = 0, width = 0, health = 4, timer = 30, creationTime
 
+// Remove o ? recebido junto a URL. Linha 28 em index.html
 var level = window.location.search
 level = level.replace('?', '')
 
+// Seleciona o level
 if (level === 'normal') creationTime = 1250
 else if (level === 'hard') creationTime = 1000
 else if (level === 'insane') creationTime = 850
 
+// Atribuindo o valor da altura e largura em px da janela, também quando ajustada. Linha 22 em app.index
 function areaAdjust() {
     height = window.innerHeight
     width = window.innerWidth
 }
 areaAdjust()
 
+// Cronometro
 var stopwatch = setInterval(function () {
 
     if (timer <= 0)
@@ -23,12 +27,13 @@ var stopwatch = setInterval(function () {
     }
 }, 1000)
 
-
+ // Cria o elemento img e posições randomicas 
 function randomPosition() {
 
+    // Checa se já não exista o elemento img criado, caso exista, remove e subtrai 1 de vida
     if (document.getElementById('fly')) {
         document.getElementById('fly').remove()
-
+    
         if (health <= 0)
             window.location.href = 'gameover.html'
         else
@@ -37,13 +42,18 @@ function randomPosition() {
         health--
     }
 
+    // Cria um vaor aleatorio emtre 0 e 1 e multiplica as var width e height para criar posições aleatorias do elemento
+    // É subtraido 90px para que o elemento não sai da area da janela e crie uma barra de rolagem
     positionX = Math.floor(Math.random() * width) - 90
     positionY = Math.floor(Math.random() * height) - 90
 
+    
+    // Resolvendo o problema acima ao subtrair 90px e o elemento sumir da tela, pelo valor estar negativo em px
+    // Testa se var position é menor que 0, então var = 0, se não, mantem seu valor
     positionX = positionX < 0 ? 0 : positionX
     positionY = positionY < 0 ? 0 : positionY
 
-
+    // Criando o elemento img de id fly
     var fly = document.createElement('img')
     fly.src = './img/fly.png'
     fly.className = size() + ' ' + side()
@@ -52,14 +62,15 @@ function randomPosition() {
     fly.style.top = positionY + 'px'
     fly.style.position = 'absolute'
 
+    // Evento onclick, que ao usuário clicar no elemento, ele é removido
     fly.onclick = function () {
         fly.remove()
     }
-
+    
     document.body.appendChild(fly)
 }
 
-
+// Alterando o tamanho do elemento criado. 3 tamanhos diferentes: linha 
 function size() {
     var size = Math.floor(Math.random() * 3)
 
@@ -68,9 +79,9 @@ function size() {
         case 1: return 'fly2'
         case 2: return 'fly3'
     }
-
 }
 
+// Alterando os lados do elemento criado
 function side() {
     var side = Math.floor(Math.random() * 2)
 
@@ -78,5 +89,4 @@ function side() {
         case 0: return 'side0'
         case 1: return 'side1'
     }
-
 }
